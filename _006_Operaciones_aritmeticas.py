0df6ea8439262cc447b4e13d90c1bd531e658aaa
BryanJavierGarciaBarrios <145368570+BryanJavierGarciaBarrios@users.noreply.github.com>
2023-09-18 09:23:28 -0600

    Practica 6 Uso de operaciones aritméticas

---
+++ _006_Operaciones_aritmeticas.py
@@ -0,0 +1,9 @@
+#Las operaciones basicas utilizan +, -, *, /, para la suma, resta, multiplicacion, y disivion respectivamente
+suma=3+7
+resta=3-7
+multiplicacion=3*7
+division=6/3
+
+print(suma,resta,multiplicacion,division)
+
+
