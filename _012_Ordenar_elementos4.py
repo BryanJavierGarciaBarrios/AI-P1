e39e08a3031a15b8a9b352e297afff6caa12495c
BryanJavierGarciaBarrios <145368570+BryanJavierGarciaBarrios@users.noreply.github.com>
2023-09-18 09:31:42 -0600

    Practica 12

---
+++ _012_Ordenar_elementos4.py
@@ -0,0 +1,11 @@
+carros=["tesla","bmw","audi","toyota","ford"]
+print("Array original: ",carros)
+carros.sort() #Sort ordena los elementos de manera alfabetica (es de forma permanente)
+print(carros)
+carros.sort(reverse=True) #Lo hcae de manera inversa
+print(carros)
+
+carros=["tesla","bmw","audi","toyota","ford"]
+print(sorted(carros))
+print(carros)
+
