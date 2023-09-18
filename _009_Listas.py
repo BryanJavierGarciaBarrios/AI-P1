f05632ef4e24beca7d003962d645964a52988612
BryanJavierGarciaBarrios <145368570+BryanJavierGarciaBarrios@users.noreply.github.com>
2023-09-18 09:28:58 -0600

    Practica 9

---
+++ _009_Listas.py
@@ -0,0 +1,11 @@
+#Las listas son arrays
+carros=["audi","mercedes","tesla","toyota","bmw"]
+print(carros)
+print(carros[2]) #Para imprimir una posicion en especifico del array es necesario mencionar la posicion
+
+#Posiciones negativas
+#Las posiciones negativas hacen referencia a la manera de acceder a los elementos de un array
+#podemos acceder desde la ultima posicion hasta la primera
+#Ejemplo
+print("\nEl ultimo elemento es: ",carros[-1])
+print("El penultimo elemento es: ",carros[-2])
