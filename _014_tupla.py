142fd1dda55d948ec6f1c6535eb9294dd98852cc
BryanJavierGarciaBarrios <145368570+BryanJavierGarciaBarrios@users.noreply.github.com>
2023-09-18 09:32:20 -0600

    Practica 14

---
+++ _014_tupla.py
@@ -0,0 +1,7 @@
+lista=["fecha","hora","clima",3,4,6] #La lista se declara con corchetes
+print(lista)
+
+tupla=("Valor","Dato",4,"Operacion") #La tupla con parentesis, y no se puede modificar como la lista
+print(tupla)
+print(tupla[1]) #Tambien puedo acceder a elementos especificos de la tupla
+print(tupla[0],tupla[3]) #Para concatenar se utiliza una coma
