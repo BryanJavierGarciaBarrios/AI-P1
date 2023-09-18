87199f745cc87a3e5d1bd6a58987065865aa4dfc
BryanJavierGarciaBarrios <145368570+BryanJavierGarciaBarrios@users.noreply.github.com>
2023-09-18 09:32:42 -0600

    Practica 15

---
+++ _015_Conversiones_lista_tupla.py
@@ -0,0 +1,6 @@
+lista=[1,2,4,5,6,7]
+tupla=("Morado","Rango",4,"Objeto")
+
+numTupla= tuple(lista) #Tuple() transforma una lista en tupla
+datLista=list(tupla) #list() transforma de tupla a lista
+print(numTupla,"\n",datLista)
