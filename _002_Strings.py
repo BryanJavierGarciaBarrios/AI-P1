ca1d9e2c3a4ebb8b544fa967a34fe3b85ff0a451
BryanJavierGarciaBarrios <145368570+BryanJavierGarciaBarrios@users.noreply.github.com>
2023-09-18 09:17:40 -0600

    Practica 2 Uso de Strings

---
+++ _002_Strings.py
@@ -0,0 +1,8 @@
+variable = "No se encontraron problemas" #Una variable necesita comillas para ser declarado como string
+print(variable)
+#Para el uso de comillas dentro de un string es necesario usar dobles comillas al exterior
+variable = "No se 'encontraron' problemas" 
+print(variable)
+#O viceversa, comillas simples al exterior y dobles comillas
+variable = 'No se "encontraron problemas"'
+print(variable)
