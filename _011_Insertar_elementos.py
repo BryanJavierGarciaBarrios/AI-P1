ac49cb331cd363dddeb2cd3bfd8b32c89b9562f8
BryanJavierGarciaBarrios <145368570+BryanJavierGarciaBarrios@users.noreply.github.com>
2023-09-18 09:30:00 -0600

    Practica 11

---
+++ _011_Insertar_elementos.py
@@ -0,0 +1,9 @@
+letras=["a","b","c","d"]
+letras.append("f") #Append inserta el elemento al final del array
+print("Array original: ",letras)
+
+letras.insert(-1,"e")
+print(letras)
+letras=["a","b","c","d"]
+letras.insert(3,"e")
+print(letras)
