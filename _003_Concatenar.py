35802c695f20f2400b834cde04e328536523b2e8
BryanJavierGarciaBarrios <145368570+BryanJavierGarciaBarrios@users.noreply.github.com>
2023-09-18 09:19:03 -0600

    Practica 3 Concatenar strings

---
+++ _003_Concatenar.py
@@ -0,0 +1,10 @@
+v1="Este programa"
+v2="permite concatenar"
+v3=v1+v2 #Permite 'sumar' (concatenar) dos strings en una sola variable
+print(v3)
+
+#Para aplicar un espacio se agrega unas comillas con un espacio en blanco
+v1="Este programa"
+v2="permite concatenar"
+v3=v1+ " " + v2
+print(v3)
