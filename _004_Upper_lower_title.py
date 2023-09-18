399bc753e118c3ebf0a3423db8b88c59a133e422
BryanJavierGarciaBarrios <145368570+BryanJavierGarciaBarrios@users.noreply.github.com>
2023-09-18 09:22:08 -0600

    Practica 4

---
+++ _004_Upper_lower_title.py
@@ -0,0 +1,11 @@
+v1="hola mundo"
+v1=v1.title() #El metodo title() permite mostrar un string como titulo
+print(v1)
+
+v2="explorador de soluciones"
+#Metodo upper() convierte en mayusculas todos los caracteres dentro de un String
+print(v2.upper()) #Tambien es posible llamar a los metodos dentro del print
+
+v2="EXPLORADOR DE SOLUCIONES"
+print(v2.lower())
+
